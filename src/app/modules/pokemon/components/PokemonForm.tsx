a49d68b1661286f268938d104607eb6651e91192
Shohei Kameda <shoheik@cpan.org>
2024-12-14 14:49:15 +0900

    added pokemon

---
+++ src/app/modules/pokemon/components/PokemonForm.tsx
@@ -0,0 +1,8 @@
+export default function PokemonForm(){
+  return (
+    <form>
+      <input type="text" placeholder="Pokemon name" />
+      <button type="submit">Search</button>
+    </form>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import PokemonForm from "./modules/pokemon/components/PokemonForm";
 
 export default function Home() {
   return (
     <>
       <h1 className="text-red-500 text-base">test</h1>
+      <PokemonForm />
     </>
   );
 }
