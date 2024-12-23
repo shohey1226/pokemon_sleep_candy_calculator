652dd6132d9a9c865f2dc061ae0e18f66d4cca02
Shohei Kameda <shoheik@cpan.org>
2024-12-23 09:43:38 +0900

    added tailwind style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
-import { GlobalStateProvider } from '../context/GlobalState';
+import React from "react";
+import * as Dialog from "@radix-ui/react-dialog";
+import { GlobalStateProvider } from "../context/GlobalState";
 import PokemonForm from "../components/PokemonForm";
 import PokemonList from "../components/PokemonList";
 
 export default function Home() {
   return (
     <GlobalStateProvider>
-      <PokemonForm />    
+      <Dialog.Root >
+        <Dialog.Trigger className="px-4 py-2 bg-blue-500 text-white rounded">Open</Dialog.Trigger>
+        <Dialog.Portal>
+          <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-50" />
+          <Dialog.Content className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 rounded shadow-lg">
+            <Dialog.Close className="absolute top-2 right-2 px-4 py-2 bg-red-500 text-white rounded">x</Dialog.Close>
+            <Dialog.Title></Dialog.Title>
+              <PokemonForm />
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>      
       <PokemonList />
     </GlobalStateProvider>
   );
--- src/components/PokemonForm.tsx
+++ src/components/PokemonForm.tsx
@@ -34,100 +34,118 @@ export default function PokemonForm() {
   };
 
   return (
-    <div>
-      <div>
-        <label>現在のレベル</label>
+    <div className="p-4 bg-gray-100 rounded-lg shadow-md">
+      <div className="mb-4">
+        <label className="block text-gray-700">現在のレベル</label>
         <input
           type="number"
           name="currentLevel"
           min="1"
           max="60"
-          className="text-black"
+          className="w-full p-2 mt-1 border rounded text-black"
           value={formValues.currentLevel}
           onChange={handleChange}
         />
       </div>
-      <div>
-        <label>目標のレベル</label>
+      <div className="mb-4">
+        <label className="block text-gray-700">目標のレベル</label>
         <input
           type="number"
           name="targetLevel"
           min="1"
           max="60"
-          className="text-black"
+          className="w-full p-2 mt-1 border rounded text-black"
           value={formValues.targetLevel}
           onChange={handleChange}
         />
       </div>
-      <div>
-        <label>経験値タイプ</label>
-        <select name="expType" className="text-black" value={formValues.expType} onChange={handleChange}>
+      <div className="mb-4">
+        <label className="block text-gray-700">経験値タイプ</label>
+        <select name="expType" className="w-full p-2 mt-1 border rounded text-black" value={formValues.expType} onChange={handleChange}>
           <option value="600">600: 通常</option>
           <option value="900">900: ヨーギラス系統、ミニリュウ系統</option>
           <option value="1080">1080: ライコウ、エンテイ、スイクン</option>
         </select>
       </div>
-      <div>
-        <label>性格による経験値補正</label>
-        <div>
-          <input
-            type="radio"
-            name="expBoost"
-            value="down"
-            checked={formValues.expBoost === "down"}
-            onChange={handleChange}
-          />
-          ▼
-          <input
-            type="radio"
-            name="expBoost"
-            value="normal"
-            checked={formValues.expBoost === "normal"}
-            onChange={handleChange}
-          />
-          -
-          <input
-            type="radio"
-            name="expBoost"
-            value="up"
-            checked={formValues.expBoost === "up"}
-            onChange={handleChange}
-          />
-          ▲
+      <div className="mb-4">
+        <label className="block text-gray-700">性格による経験値補正</label>
+        <div className="flex items-center space-x-4">
+          <label className="flex items-center">
+            <input
+              type="radio"
+              name="expBoost"
+              value="down"
+              className="mr-2"
+              checked={formValues.expBoost === "down"}
+              onChange={handleChange}
+            />
+            ▼
+          </label>
+          <label className="flex items-center">
+            <input
+              type="radio"
+              name="expBoost"
+              value="normal"
+              className="mr-2"
+              checked={formValues.expBoost === "normal"}
+              onChange={handleChange}
+            />
+            -
+          </label>
+          <label className="flex items-center">
+            <input
+              type="radio"
+              name="expBoost"
+              value="up"
+              className="mr-2"
+              checked={formValues.expBoost === "up"}
+              onChange={handleChange}
+            />
+            ▲
+          </label>
         </div>
-        <div>
-          <input
-            type="radio"
-            name="boostEvent"
-            value="none"
-            checked={formValues.boostEvent === "none"}
-            onChange={handleChange}
-          />
-          通常
-          <input
-            type="radio"
-            name="boostEvent"
-            value="miniBoost"
-            checked={formValues.boostEvent === "miniBoost"}
-            onChange={handleChange}
-          />
-          ミニブースト
-          <input
-            type="radio"
-            name="boostEvent"
-            value="boost"
-            checked={formValues.boostEvent === "boost"}
-            onChange={handleChange}
-          />
-          ブースト
+        <div className="flex items-center space-x-4 mt-2">
+          <label className="flex items-center">
+            <input
+              type="radio"
+              name="boostEvent"
+              value="none"
+              className="mr-2"
+              checked={formValues.boostEvent === "none"}
+              onChange={handleChange}
+            />
+            通常
+          </label>
+          <label className="flex items-center">
+            <input
+              type="radio"
+              name="boostEvent"
+              value="miniBoost"
+              className="mr-2"
+              checked={formValues.boostEvent === "miniBoost"}
+              onChange={handleChange}
+            />
+            ミニブースト
+          </label>
+          <label className="flex items-center">
+            <input
+              type="radio"
+              name="boostEvent"
+              value="boost"
+              className="mr-2"
+              checked={formValues.boostEvent === "boost"}
+              onChange={handleChange}
+            />
+            ブースト
+          </label>
         </div>
       </div>
-      <div>
-        <p>Candy Required: {outValues?.calcRequiredCandy}</p>
-        <p>Dream Shards Required: {outValues?.calcRequiredDreamShards}</p>
-        <p>Experience Required: {outValues?.calcRequiredExp}</p>
+      <div className="mb-4">
+        <p className="text-gray-700">Candy Required: {outValues?.calcRequiredCandy}</p>
+        <p className="text-gray-700">Dream Shards Required: {outValues?.calcRequiredDreamShards}</p>
+        <p className="text-gray-700">Experience Required: {outValues?.calcRequiredExp}</p>
       </div>
-      <button onClick={()=> dispatch({ type: 'ADD_POKEMON', payload: outValues })}>Add Pokemon</button>
+      <button className="w-full p-2 bg-blue-500 text-white rounded" onClick={()=> dispatch({ type: 'ADD_POKEMON', payload: outValues })}>Add Pokemon</button>
     </div>
   );
 }
