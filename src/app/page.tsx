adae4914968dd86fd40f9122787401644186e98c
Shohei Kameda <shoheik@cpan.org>
2024-12-23 13:38:24 +0900

    updated style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,11 @@ export default function Home() {
         <Dialog.Portal>
           <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-50" />
           <Dialog.Content className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 rounded shadow-lg">
-            <Dialog.Close className="absolute top-2 right-2 px-4 py-2 bg-red-500 text-white rounded">x</Dialog.Close>
+            <Dialog.Close className="absolute top-2 right-2 px-2 py-2 text-black rounded">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </Dialog.Close>
             <Dialog.Title></Dialog.Title>
               <PokemonForm />
           </Dialog.Content>
--- src/components/PokemonForm.tsx
+++ src/components/PokemonForm.tsx
@@ -34,42 +34,42 @@ export default function PokemonForm() {
   };
 
   return (
-    <div className="p-4 bg-gray-100 rounded-lg shadow-md">
-      <div className="mb-4">
-        <label className="block text-gray-700">現在のレベル</label>
+    <div className="">
+      <div className="mb-6">
+        <label className="block text-gray-800 font-semibold">現在のレベル</label>
         <input
           type="number"
           name="currentLevel"
           min="1"
           max="60"
-          className="w-full p-2 mt-1 border rounded text-black"
+          className="w-full p-3 mt-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
           value={formValues.currentLevel}
           onChange={handleChange}
         />
       </div>
-      <div className="mb-4">
-        <label className="block text-gray-700">目標のレベル</label>
+      <div className="mb-6">
+        <label className="block text-gray-800 font-semibold">目標のレベル</label>
         <input
           type="number"
           name="targetLevel"
           min="1"
           max="60"
-          className="w-full p-2 mt-1 border rounded text-black"
+          className="w-full p-3 mt-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
           value={formValues.targetLevel}
           onChange={handleChange}
         />
       </div>
-      <div className="mb-4">
-        <label className="block text-gray-700">経験値タイプ</label>
-        <select name="expType" className="w-full p-2 mt-1 border rounded text-black" value={formValues.expType} onChange={handleChange}>
+      <div className="mb-6">
+        <label className="block text-gray-800 font-semibold">経験値タイプ</label>
+        <select name="expType" className="w-full p-3 mt-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" value={formValues.expType} onChange={handleChange}>
           <option value="600">600: 通常</option>
           <option value="900">900: ヨーギラス系統、ミニリュウ系統</option>
           <option value="1080">1080: ライコウ、エンテイ、スイクン</option>
         </select>
       </div>
-      <div className="mb-4">
-        <label className="block text-gray-700">性格による経験値補正</label>
-        <div className="flex items-center space-x-4">
+      <div className="mb-6">
+        <label className="block text-gray-800 font-semibold">性格による経験値補正</label>
+        <div className="flex items-center space-x-4 mt-2">
           <label className="flex items-center">
             <input
               type="radio"
@@ -104,7 +104,7 @@ export default function PokemonForm() {
             ▲
           </label>
         </div>
-        <div className="flex items-center space-x-4 mt-2">
+        <div className="flex items-center space-x-4 mt-4">
           <label className="flex items-center">
             <input
               type="radio"
@@ -140,12 +140,12 @@ export default function PokemonForm() {
           </label>
         </div>
       </div>
-      <div className="mb-4">
-        <p className="text-gray-700">Candy Required: {outValues?.calcRequiredCandy}</p>
-        <p className="text-gray-700">Dream Shards Required: {outValues?.calcRequiredDreamShards}</p>
-        <p className="text-gray-700">Experience Required: {outValues?.calcRequiredExp}</p>
+      <div className="mb-6">
+        <p className="text-gray-800 font-semibold">Candy Required: <span className="font-normal">{outValues?.calcRequiredCandy}</span></p>
+        <p className="text-gray-800 font-semibold">Dream Shards Required: <span className="font-normal">{outValues?.calcRequiredDreamShards}</span></p>
+        <p className="text-gray-800 font-semibold">Experience Required: <span className="font-normal">{outValues?.calcRequiredExp}</span></p>
       </div>
-      <button className="w-full p-2 bg-blue-500 text-white rounded" onClick={()=> dispatch({ type: 'ADD_POKEMON', payload: outValues })}>Add Pokemon</button>
+      <button className="w-full p-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500" onClick={()=> dispatch({ type: 'ADD_POKEMON', payload: outValues })}>Add Pokemon</button>
     </div>
   );
 }
