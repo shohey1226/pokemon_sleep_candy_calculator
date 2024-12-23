cd7a444602597b8bf56aad4949aa351e5e395af8
Shohei Kameda <shoheik@cpan.org>
2024-12-23 16:25:23 +0900

    updated UI

--- src/components/PokemonForm.tsx
+++ src/components/PokemonForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect, useContext } from "react";
-import { GlobalStateContext } from '../context/GlobalState';
+import { GlobalStateContext } from "../context/GlobalState";
 import { calculateCandy } from "@/lib/calculateCandy";
 import { FormValues, OutValues } from "@/types";
 
@@ -14,12 +14,12 @@ export default function PokemonForm() {
     expType: "600",
     expBoost: "normal",
     boostEvent: "none",
-    name: null,
+    name: ""
   });
 
   useEffect(() => {
     console.log("formValues", formValues);
-    const candy = calculateCandy(formValues);    
+    const candy = calculateCandy(formValues);
     setNumberOfCandy(candy);
   }, [formValues]);
 
@@ -37,14 +37,7 @@ export default function PokemonForm() {
   return (
     <div className="">
       <div className="mb-6">
-        <label className="block text-gray-800 font-semibold">名前</label>
-        <input
-          type="text"
-          name="name"
-          className="w-full p-3 mt-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-          value={formValues.name || ""}
-          onChange={handleChange}
-        />
+
         <label className="block text-gray-800 font-semibold">現在のレベル</label>
         <input
           type="number"
@@ -70,7 +63,12 @@ export default function PokemonForm() {
       </div>
       <div className="mb-6">
         <label className="block text-gray-800 font-semibold">経験値タイプ</label>
-        <select name="expType" className="w-full p-3 mt-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" value={formValues.expType} onChange={handleChange}>
+        <select
+          name="expType"
+          className="w-full p-3 mt-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          value={formValues.expType}
+          onChange={handleChange}
+        >
           <option value="600">600: 通常</option>
           <option value="900">900: ヨーギラス系統、ミニリュウ系統</option>
           <option value="1080">1080: ライコウ、エンテイ、スイクン</option>
@@ -148,13 +146,40 @@ export default function PokemonForm() {
             ブースト
           </label>
         </div>
+        <label className="mt-5 block text-gray-800 font-semibold">コメント</label>
+        <input
+          type="text"
+          name="name"
+          className="w-full p-3 mt-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          value={formValues.name}
+          onChange={handleChange}
+        />
       </div>
       <div className="mb-6">
-        <p className="text-gray-800 font-semibold">Candy Required: <span className="font-normal">{outValues?.calcRequiredCandy}</span></p>
-        <p className="text-gray-800 font-semibold">Dream Shards Required: <span className="font-normal">{outValues?.calcRequiredDreamShards}</span></p>
-        <p className="text-gray-800 font-semibold">Experience Required: <span className="font-normal">{outValues?.calcRequiredExp}</span></p>
+        <p className="text-gray-800 font-semibold">
+          Candy Required: <span className="font-normal">{outValues?.calcRequiredCandy}</span>
+        </p>
+        <p className="text-gray-800 font-semibold">
+          Dream Shards Required: <span className="font-normal">{outValues?.calcRequiredDreamShards}</span>
+        </p>
+        <p className="text-gray-800 font-semibold">
+          Experience Required: <span className="font-normal">{outValues?.calcRequiredExp}</span>
+        </p>
       </div>
-      <button className="w-full p-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500" onClick={()=> dispatch({ type: 'ADD_POKEMON', payload: outValues })}>Add Pokemon</button>
+      <button
+        className="flex justify-center w-full p-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        onClick={() => dispatch({ type: "ADD_POKEMON", payload: outValues })}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-4 w-4"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
+        </svg>
+      </button>
     </div>
   );
 }
