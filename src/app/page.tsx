2cee06af2607895589af95bf8f5576355b44a36a
Shohei Kameda <shoheik@cpan.org>
2024-12-24 11:31:03 +0900

    move GlobalStateProvider to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-"use client";
-
 import React from "react";
-import { GlobalStateProvider } from "../context/GlobalState";
 import PokemonList from "../components/PokemonList";
 import PokemonModal from "../components/PokemonModal";
 
 export default function Home() {
   return (
-    <GlobalStateProvider>
       <div className="container mx-auto">
         <div className="flex items-center justify-center">
           <h1 className="my-5 text-xl font-bold">ポケスリ飴計算機</h1>        
@@ -19,6 +15,5 @@ export default function Home() {
           <PokemonList />
         </div>
       </div>
-    </GlobalStateProvider>
   );
 }
